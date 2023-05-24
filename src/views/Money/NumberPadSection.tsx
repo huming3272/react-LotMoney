c6a347f7861ab9ac11c3c6b19bcf0331f94c4888
huming <1043005538@qq.com>
2023-05-24 13:50:14 +0800

    用表驱动优化逻辑

--- src/views/Money/NumberPadSection.tsx
+++ src/views/Money/NumberPadSection.tsx
@@ -2,92 +2,138 @@ import styled from "styled-components";
 import React, {useState} from "react";
 
 const Wrapper = styled.section`
-  display:flex;
+  display: flex;
   flex-direction: column;
-  > .output{
-    background:white;
+
+  > .output {
+    background: white;
     font-size: 36px;
     line-height: 72px;
-    text-align:right;
+    text-align: right;
     padding: 0 16px;
-    box-shadow: inset 0 -5px 5px -5px rgba(0,0,0,0.25),
-    inset 0 5px 5px -5px rgba(0,0,0,0.25);
+    box-shadow: inset 0 -5px 5px -5px rgba(0, 0, 0, 0.25),
+    inset 0 5px 5px -5px rgba(0, 0, 0, 0.25);
   }
-  > .pad{
-    > button{
-      font-size: 18px; float: left; width: 25%; height: 64px; border: none;
-      &:hover{cursor:pointer}
-      &.ok{ height: 128px; float: right; }
-      &.zero{ width: 50%; }
-      &:nth-child(1){
-        background:#f2f2f2;
+
+  > .pad {
+    > button {
+      font-size: 18px;
+      float: left;
+      width: 25%;
+      height: 64px;
+      border: none;
+
+      &:hover {
+        cursor: pointer
       }
+
+      &.ok {
+        height: 128px;
+        float: right;
+      }
+
+      &.zero {
+        width: 50%;
+      }
+
+      &:nth-child(1) {
+        background: #f2f2f2;
+      }
+
       &:nth-child(2),
       &:nth-child(5) {
-        background:#E0E0E0;
+        background: #E0E0E0;
       }
+
       &:nth-child(3),
       &:nth-child(6),
       &:nth-child(9) {
-        background:#D3D3D3;
+        background: #D3D3D3;
       }
+
       &:nth-child(4),
       &:nth-child(7),
       &:nth-child(10) {
-        background:#C1C1C1;
+        background: #C1C1C1;
       }
+
       &:nth-child(8),
       &:nth-child(11),
       &:nth-child(13) {
-        background:#B8B8B8;
+        background: #B8B8B8;
       }
+
       &:nth-child(12) {
-        background:#9A9A9A;
+        background: #9A9A9A;
       }
+
       &:nth-child(14) {
-        background:#A9A9A9;
+        background: #A9A9A9;
       }
     }
   }
 `
-const NumberPadSection:React.FunctionComponent = ()=>{
+const NumberPadSection: React.FunctionComponent = () => {
     // as在typescript是强制指定类型的意思
     const [output, setOutput] = useState('0')
     // 给默认事件定义类型为React的鼠标事件
-    const NumberPadSection = (e: React.MouseEvent)=>{
-        const currentButton =(e.target as HTMLButtonElement).textContent as string
-        const numMap = '0123456789.'
-        // 如果是初值，并且不为点
-        if(output === '0' &&  currentButton !== '.'){
-
-            setOutput(currentButton)
-        }else{
-            // 如果是键盘值包括点
-            if(numMap.indexOf(currentButton)>-1){
-                // 如果重复hasDot就是true
-                const dotIndex = output.indexOf('.')
-                // const floatLimit = output.slice(dotIndex)
-                // console.log(floatLimit)
-                const floatLimit = output.slice(dotIndex).length <3
-                console.log(floatLimit)
-                if(dotIndex>-1 && currentButton === '.'){
-                    return
-                }else if(floatLimit){
-                    setOutput(output+currentButton)
-                }
-            //    其他的是删除、清空、OK
-            }else{
-
-            }
+    const NumberPadSection = (e: React.MouseEvent) => {
+        const currentButton = (e.target as HTMLButtonElement).textContent as string
+        const numPad = '0123456789'
+        let key: string
+        if (numPad.indexOf(currentButton) > -1) {
+            key = 'num'
+        } else {
+            key = currentButton
         }
-
+        const dotIndex = output.indexOf('.')
+        const floatLimit = output.slice(dotIndex).length < 3
+        const lengthLimit = output.slice(0, dotIndex).length
+        
+        //用表驱动优化逻辑
+        // 加上类型约束
+        const numMap: { [key: string]: Function } = {
+            '.': () => {
+                if (dotIndex > -1 && currentButton === '.') {
+                    return
+                } else if (floatLimit) {
+                    setOutput(output + currentButton)
+                }
+            },
+            // 如果是键盘值
+            'num': () => {
+                // 如果是初值
+                if (output === '0') {
+                    setOutput(currentButton)
+                } else if (floatLimit && lengthLimit < 9) {
+                    setOutput(output + currentButton)
+                }
+            },
+            '删除': () => {
+                if (output.length === 1) {
+                    setOutput('0')
+                } else {
+                    setOutput(output.slice(0, output.length - 1))
+                }
+            },
+            '清空': () => {
+                setOutput('0')
+            },
+            'OK': () => {
+                console.log(output)
+            },
+        }
+        numMap[key]?.()
+        // 如果是初值，并且不为点或者0
     }
-    return(
+    return (
         <Wrapper>
             <div className="output">
                 {output}
             </div>
-            <div className="pad clearfix" onClick={(e)=>{NumberPadSection(e)}}>
+            <div className="pad clearfix" onClick={(e) => {
+                NumberPadSection(e)
+            }}>
                 <button>1</button>
                 <button>2</button>
                 <button>3</button>
