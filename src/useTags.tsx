00b8bfa19bb97ad6483979452926dca0dd44a3c3
huming <1043005538@qq.com>
2023-05-25 18:40:05 +0800

    tag list 样式做完

--- src/useTags.tsx
+++ src/useTags.tsx
@@ -1,6 +1,7 @@
 // 定义类型为字符串组成的数组
 import React from "react";
 const useTags = ()=>{
+    // 封装一个自定义 Hook
     const [tags, setTags] = React.useState<string[]>(['穿', '吃', '房', '交通'])
     return {tags, setTags}
 }
