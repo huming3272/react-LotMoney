10dfae3097fda5252d5049b8fa52a99b8f48a17c
huming <1043005538@qq.com>
2023-05-26 20:29:07 +0800

    增加删除 tag功能

--- src/useTags.tsx
+++ src/useTags.tsx
@@ -32,8 +32,20 @@ const useTags = ()=>{
         const tagsClone = JSON.parse(JSON.stringify(tags))
         // 把tagsClone的第index删除，换成{id:id, name: obj.name}
         tagsClone.splice(index,1,{id:id, name:obj.name});
+        // react强调不可变数据，所以要用最新数据替换
         setTags(tagsClone);
     }
-    return {tags, setTags, findTag, updateTag, findTagIndex};
+    const deleteTag = (id: number) =>{
+    //    获取你要删除的tag的下标
+        const index = findTagIndex(id)
+    //    深拷贝tags得到tagsClone
+        const tagsClone = JSON.parse(JSON.stringify(tags))
+    //    把tagsClone的第index删掉
+        tagsClone.splice(index,1);
+        // 删掉以后再更新数据
+        setTags(tagsClone);
+
+    }
+    return {tags, setTags, findTag, updateTag, findTagIndex,deleteTag};
 }
 export {useTags}
--- src/views/Tag.tsx
+++ src/views/Tag.tsx
@@ -26,10 +26,30 @@ const InputWrapper = styled.div`
   margin-top: 8px;
 `;
 const Tag: React.FunctionComponent = (props) => {
-    const {findTag,updateTag} = useTags();
+    const {findTag,updateTag, deleteTag} = useTags();
     // 可以以对象形式获取路由的参数,定义idString为useParams的id
     let {id: idString} = useParams<Params>()
     const tag = findTag(parseInt(idString))
+    const tagContent = (tag: {id:number; name: string}) =>(
+        <div>
+            <InputWrapper>
+                <Input label="标签名" type="text" placeholder="标签名"
+                       value={tag.name}
+                       onChange={(e) => {
+                           updateTag(tag.id, {name: e.target.value});
+                       }}
+                />
+            </InputWrapper>
+            <Center>
+                <Space/>
+                <Space/>
+                <Space/>
+                <Button onClick={() => {
+                    deleteTag(tag.id);
+                }}>删除标签</Button>
+            </Center>
+        </div>
+    );
     return (
         <Layout>
             <Topbar>
@@ -37,20 +57,7 @@ const Tag: React.FunctionComponent = (props) => {
                 <span>编辑标签</span>
                 <Icon/>
             </Topbar>
-            <InputWrapper>
-                <Input label='标签名' type="text" placeholder="标签名"
-                        value = {tag.name}
-                       // 监听change方法
-                       onChange={(e) => {
-                           updateTag(tag.id, {name:e.target.value})
-                       }}
-                ></Input>
-            </InputWrapper>
-            <Center>
-                <Space></Space>
-                <Space></Space>
-                <Button>删除标签</Button>
-            </Center>
+            {tag ? tagContent(tag): <Center>tag 不存在</Center>}
         </Layout>
     );
 };
