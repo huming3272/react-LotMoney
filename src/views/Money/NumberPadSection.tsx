5110a0c486d694c2d6cd89fc9f8f18d840b81c32
huming <1043005538@qq.com>
2023-06-07 21:14:52 +0800

    增加了随机键盘生成的功能

--- src/views/Money/NumberPadSection.tsx
+++ src/views/Money/NumberPadSection.tsx
@@ -10,7 +10,44 @@ type Props = {
 }
 
 const NumberPadSection: React.FunctionComponent<Props> = (props) => {
-
+    const [padStr,setPadStr] = React.useState<string>('')
+    // const memoizedPadGenerator = React.useCallback(() => {
+    //      padGenerator()
+    // },[])
+    // 随机给键盘生成的函数
+    React.useEffect(()=>{
+        // 生成数字键一共十个
+        const padGenerator = () => {
+            let countStr = ''
+            const result:string[] = []
+            for(let i = 0; i< 10;i++){
+                let randomNum = String(Math.floor((Math.random()*10)))
+                if(i === 0){
+                    result.push(randomNum)
+                }else{
+                    while(result.indexOf(randomNum) > -1){
+                        randomNum = String(Math.floor((Math.random()*10)))
+                    }
+                    result.push(randomNum)
+                }
+            }
+            // 插入删除、清空、OK等
+            result.splice(3,0,'删除')
+            result.splice(7,0,'清空')
+            result.splice(11,0,'<button className="ok" style="height: 128px;float: right;">OK</button>')
+            const zeroPosition = result.splice(12,1)
+            result.splice(12,0,`<button className="zero" style="width:50%">${zeroPosition}</button>`)
+            result.push(`<button className="dot">.</button>`)
+            result.forEach((ele,index,group) =>{
+                if(ele.length <3){
+                    group[index] = `<button>${ele}</button>`
+                }
+                countStr = countStr + group[index]
+            })
+            setPadStr(countStr)
+        }
+        padGenerator()
+    },[])
     // const [output, setOutput] = useState('0')
     const output = props.value
     // 给默认事件定义类型为React的鼠标事件
@@ -25,28 +62,30 @@ const NumberPadSection: React.FunctionComponent<Props> = (props) => {
             props.onOK?.()
         }
     }
+
     return (
         <Wrapper>
             <div className="output">
                 {output}
             </div>
-            <div className="pad clearfix" onClick={(e) => {
+            {/*dangerouslySetInnerHTML={{__html: padStr}}*/}
+            <div className="pad clearfix" dangerouslySetInnerHTML={{__html: padStr}} onClick={(e) => {
                 onClickButtonWrapper(e)
             }}>
-                <button>1</button>
-                <button>2</button>
-                <button>3</button>
-                <button>删除</button>
-                <button>4</button>
-                <button>5</button>
-                <button>6</button>
-                <button>清空</button>
-                <button>7</button>
-                <button>8</button>
-                <button>9</button>
-                <button className="ok">OK</button>
-                <button className="zero">0</button>
-                <button className="dot">.</button>
+                {/*<button>1</button>*/}
+                {/*<button>2</button>*/}
+                {/*<button>3</button>*/}
+                {/*<button>删除</button>*/}
+                {/*<button>4</button>*/}
+                {/*<button>5</button>*/}
+                {/*<button>6</button>*/}
+                {/*<button>清空</button>*/}
+                {/*<button>7</button>*/}
+                {/*<button>8</button>*/}
+                {/*<button>9</button>*/}
+                {/*<button className="ok">OK</button>*/}
+                {/*<button className="zero">0</button>*/}
+                {/*<button className="dot">.</button>*/}
             </div>
         </Wrapper>
     )
