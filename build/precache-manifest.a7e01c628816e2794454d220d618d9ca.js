7bf48f94989e3e82994f33cbc268e15e09ef23cd
huming <1043005538@qq.com>
2023-06-12 16:34:33 +0800

    给package.json加homepage确定主页

---
+++ build/precache-manifest.a7e01c628816e2794454d220d618d9ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76ca4950862a3410a2f5da36b2f37d01",
+    "url": "/react-LotMoney/index.html"
+  },
+  {
+    "revision": "3d06fd6ba9842c4bd5cb",
+    "url": "/react-LotMoney/static/css/main.1f5e526f.chunk.css"
+  },
+  {
+    "revision": "120a58db20ef33902f16",
+    "url": "/react-LotMoney/static/js/2.71ea9f82.chunk.js"
+  },
+  {
+    "revision": "99406820fc9e02f862dbd66e126ca114",
+    "url": "/react-LotMoney/static/js/2.71ea9f82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d06fd6ba9842c4bd5cb",
+    "url": "/react-LotMoney/static/js/main.10c8d776.chunk.js"
+  },
+  {
+    "revision": "cecff2e15c8c01118fa7",
+    "url": "/react-LotMoney/static/js/runtime-main.330ab151.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.44bc3f985c09a74a1266bb8a69023cf0.js"
+  "/react-LotMoney/precache-manifest.a7e01c628816e2794454d220d618d9ca.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-LotMoney/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
